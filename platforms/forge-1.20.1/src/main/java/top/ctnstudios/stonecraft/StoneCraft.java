6495d61faf27198147886b18c0a6b9bb067cd190
RawDiamondMC <RawDiamondMC@outlook.com>
2023-10-06 21:37:52 +0800

    Add items in forge

--- platforms/forge-1.20.1/src/main/java/top/ctnstudios/stonecraft/StoneCraft.java
+++ platforms/forge-1.20.1/src/main/java/top/ctnstudios/stonecraft/StoneCraft.java
@@ -7,13 +7,16 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import top.ctnstudios.stonecraft.registry.Items;
+
 @Mod("stonecraft")
 public class StoneCraft {
-
+    public static String MODID = "stonecraft";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public StoneCraft() {
         IEventBus MOD_BUS = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
+        Items.init();
     }
 }
