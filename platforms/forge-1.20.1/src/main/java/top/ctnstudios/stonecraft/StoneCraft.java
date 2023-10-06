9b1f8e3ce6baea4245a7418aa39cb573bae69984
RawDiamondMC <RawDiamondMC@outlook.com>
2023-10-06 14:44:52 +0800

    Fix build

--- platforms/forge-1.20.1/src/main/java/top/ctnstudios/stonecraft/StoneCraft.java
+++ platforms/forge-1.20.1/src/main/java/top/ctnstudios/stonecraft/StoneCraft.java
@@ -1,14 +1,8 @@
 package top.ctnstudios.stonecraft;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- platforms/forge-1.20.1/src/main/java/top/ctnstudios/stonecraft/mixin/StoneCraftMixin.java
+++ platforms/forge-1.20.1/src/main/java/top/ctnstudios/stonecraft/mixin/StoneCraftMixin.java
@@ -1,15 +1,16 @@
 package top.ctnstudios.stonecraft.mixin;
 
+import net.minecraft.server.MinecraftServer;
+
 import top.ctnstudios.stonecraft.StoneCraft;
-import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(MinecraftServer.class)
 public class StoneCraftMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
+    @Inject(at = @At("HEAD"), method = "loadWorld")
     private void init(CallbackInfo info) {
         StoneCraft.LOGGER.info("This line is printed by an example mod mixin!");
     }
