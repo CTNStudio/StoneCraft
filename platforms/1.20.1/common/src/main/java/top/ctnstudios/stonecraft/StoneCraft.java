c1fb857c04b059c2a48a8a404ca0242a7ccd97fa
RawDiamondMC <RawDiamondMC@outlook.com>
2023-10-30 00:06:56 +0800

    Fix

---
+++ platforms/1.20.1/common/src/main/java/top/ctnstudios/stonecraft/StoneCraft.java
@@ -0,0 +1,7 @@
+package top.ctnstudios.stonecraft;
+
+public class StoneCraft {
+    public static final String MODID = "stonecraft";
+    public static void init() {
+    }
+}
--- platforms/1.20.1/fabric/src/main/java/top/ctnstudios/stonecraft/fabric/StoneCraftFabric.java
+++ platforms/1.20.1/fabric/src/main/java/top/ctnstudios/stonecraft/fabric/StoneCraftFabric.java
@@ -1,7 +1,9 @@
 package top.ctnstudios.stonecraft.fabric;
 
-import top.ctnstudios.stonecraft.StoneCraft;
 import net.fabricmc.api.ModInitializer;
+
+import top.ctnstudios.stonecraft.StoneCraft;
+import top.ctnstudios.stonecraft.fabriclike.StoneCraftFabricLike;
 
 public class StoneCraftFabric implements ModInitializer {
     @Override
