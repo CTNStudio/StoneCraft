72b0819b9e7b0acb5769305497073d8c7f1bca99
Amarok <91077200+AmarokIce@users.noreply.github.com>
2023-12-11 12:10:28 +0800

    Tool Material

--- common/src/main/java/top/ctnstudios/stonecraft/init/MaterialTool.java
+++ common/src/main/java/top/ctnstudios/stonecraft/init/MaterialTool.java
@@ -5,8 +5,11 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 
 public enum MaterialTool implements Tier {
-    // TODO
-    STONE_LV_1(175, 5.0f, 1.5f, 1, 7)
+    STONE_LV_1(175, 5.0f, 1.5f, 1, 7, ItemInit.LV1STONE.get()),
+    STONE_LV_2(227, 5.0f, 1.5f, 1, 7, ItemInit.LV2STONE.get()),
+    STONE_LV_3(1550, 6.0f, 2.5f, 1, 6, ItemInit.LV3STONE.get()),
+    STONE_LV_4(2753, 11.0f, 4.5f, 1, 4, ItemInit.LV4STONE.get()),
+    STONE_LV_5(3254, 15.0f, 5.0f, 1, 2, ItemInit.LV5STONE.get())
     ;
 
     private final int durability;
